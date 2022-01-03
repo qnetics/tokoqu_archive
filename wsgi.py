3a23e4304ff345d70d7fcc330540504a08f8f3b6
belajarqywok <belajarqywok@gmail.com>
2022-01-03 14:25:28 +0700

    first

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,7 @@ from werkzeug.middleware.profiler import ProfilerMiddleware
 
 # run the application
 if __name__ == "__main__":
-    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[5], profile_dir='./profile')
-    serve(app, port = 80 )
+    # app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[5], profile_dir='./profile')
+    # serve(app, port = 80 )
+
+    app.run(port=80)
