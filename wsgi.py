785c4adc872b13298cc94982ad000ffa5b2831b3
belajarqywok <belajarqywok@gmail.com>
2022-01-03 15:52:50 +0700

    first

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 import os
 from src.routes import app
+# from waitress import serve
 from werkzeug.middleware.profiler import ProfilerMiddleware
 
 # run the application
 if __name__ == "__main__":
     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[5], profile_dir='./profile')
-    app.run(threaded=True, host="0.0.0.0", port=int(os.environ.get('PORT',5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT',5000)), threaded=True)
