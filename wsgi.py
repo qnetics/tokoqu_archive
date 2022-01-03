e226cbc1693d3fc4d123f3c78ccf1447729d74ea
belajarqywok <belajarqywok@gmail.com>
2022-01-03 14:47:22 +0700

    first

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from threading import Thread
 from src.routes import app
-from waitress import serve
-from werkzeug.middleware.profiler import ProfilerMiddleware
+# from waitress import serve
+# from werkzeug.middleware.profiler import ProfilerMiddleware
 
 # run the application
 if __name__ == "__main__":
-    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[5], profile_dir='./profile')
-    serve(app)
+    # app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[5], profile_dir='./profile')
+    # serve(app)
+    app.run()
